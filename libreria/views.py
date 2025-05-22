fee36f94388b1555cadb3230ee49663f31d276b1
Williams <william25081gmail.com>
2025-05-22 17:43:47 -0600

    Se crea el html y se soloca el Doctype y en views se crea la función para buscar el html

--- libreria/views.py
+++ libreria/views.py
@@ -6,3 +6,6 @@ from django.http import HttpResponse
 #Creamos la función que envía la solictud y responde con el texto
 def inicio(request):
     return HttpResponse("<h1>Bienvenido a la Liberia</h1>")
+#Creamos la función que busca el archivo nosotros .html
+def nosotros(request):
+    return render(request, 'paginas/nosotros.html')
