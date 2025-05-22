2ddeeebad25c4d339dc7eb57fc66cefd03c55d6b
Williams <william25081gmail.com>
2025-05-22 16:30:00 -0600

    Se comenta el archivo

--- libreria/urls.py
+++ libreria/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+# Cuando el usuario ingrese a la url estara en inicio
 urlpatterns = [
-    path('', views.inicio, name='index'),
+    path('', views.inicio, name='inicio'),
 ]
