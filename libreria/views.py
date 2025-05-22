8840b79910bb4c09058b763a254ccb8d1aa94954
Williams <william25081gmail.com>
2025-05-22 16:19:47 -0600

    Se importa el HttpResponse y se crea la función del saludo

--- libreria/views.py
+++ libreria/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+# Se importa el Http
+from django.http import HttpResponse
 # Create your views here.
+
+#Creamos la función que envía la solictud y responde con el texto
+def inicio(request):
+    return HttpResponse("<h1>Bienvenido a la Liberia</h1>")
