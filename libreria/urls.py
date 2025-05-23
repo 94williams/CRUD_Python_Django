bd43d8aacf702b435d71b7d8c48b8e40bb487dfd
Williams <william25081gmail.com>
2025-05-22 18:42:06 -0600

    Se agrega el path del html nosotros en el  archivo urls.py de la app

--- libreria/urls.py
+++ libreria/urls.py
@@ -4,4 +4,6 @@ from . import views
 # Cuando el usuario ingrese a la url estara en inicio
 urlpatterns = [
     path('', views.inicio, name='inicio'),
+# Con esto accedemos vía url y mostraremos el hml de nosotros
+    path('nosotros', views.nosotros, name='nosotros')
 ]
